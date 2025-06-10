4251eac82f3e334fd702819b42a594c459b119ee
seonglae <sungle3737@gmail.com>
2025-06-11 00:28:26 +0100

    test: dom testing to vitest with jsdom

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
-    setupFiles: 'jsdom-global/register',
-    include: ['test/**/*.{js,ts}'],
-    exclude: ['test/helpers.js'],
     globals: true,
+    environment: 'jsdom',
+    include: ['test/**/*.test.ts'],
   },
 })
