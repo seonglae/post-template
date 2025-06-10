05d9d4647d632eb3ae7e8b7101efd240ee38e216
seonglae <sungle3737@gmail.com>
2025-06-11 00:01:55 +0100

    refactor: example to public with vite build

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
-    setupFiles: 'jsdom-global/register',
-    include: ['test/**/*.{js,ts}'],
-    exclude: ['test/helpers.js'],
+    environment: "jsdom",
+    setupFiles: "jsdom-global/register",
+    include: ["test/**/*.{js,ts}"],
+    exclude: ["test/helpers.js"],
     globals: true
   }
-});
+})
